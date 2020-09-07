43bd73e92f14c9d495864d803198683c3a520d89
ClementsWayne <49271496+ClementsWayne@users.noreply.github.com>
2020-09-07 21:10:49 +0200

    Update listit.sh

--- listit.sh
+++ listit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-Create a file called directories.list that contains the directory names only of the current directory.
-Add a line at the beginning of the directories.list file that reads "line one's line".
-Output the first three lines of directories.list on the console.
-Accept an integer parameter when executed and repeat the previous question's output x amount of times based on the parameter provided at execution.
+#Create a file called directories.list that contains the directory names only of the current directory.
+#Add a line at the beginning of the directories.list file that reads "line one's line".
+#Output the first three lines of directories.list on the console.
+#Accept an integer parameter when executed and repeat the previous question's output x amount of times based on the parameter provided at execution.
 
 working_dir=`pwd`
 echo "$pwd" > directories.list
